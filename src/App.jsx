3c33a424cff79b0467df4fa994ab0b5d5019d56a
rAel-Ael <alediru9267@gmail.com>
2026-05-04 17:38:15 +0200

    inserite le card, adesso procedo a mettere il form

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [articoli, setArticoli] = useState([
+    { id: 1, title: "Articolo 1", content: "Contenuto 1" },
+    { id: 2, title: "Articolo 2", content: "Contenuto 2" },
+    { id: 3, title: "Articolo 3", content: "Contenuto 3" },
+    { id: 4, title: "Articolo 4", content: "Contenuto 4" }
+  ])
+
+    return (
+    <div>
+      {articoli.map((articolo) => (
+        <div className="card mb-3" style={{ width: "18rem" }} key={articolo.id}>
+          <div className="card-body">
+            <h5 className="card-title">{articolo.title}</h5>
+            <p className="card-text">{articolo.content}</p>
+          </div>
+          <div class="card-body">
+        <a href="#" class="card-link">Articolo link</a>
+        </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default App;
